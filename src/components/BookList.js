cf542b522b7cc62032d7fd380ca36a68b226d450
sincerity628 <14785481094@163.com>
2019-12-16 19:36:44 +0800

    first init.

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>夏天，烟火和我的尸体</li>
+          <li>me before you</li>
+          <li>one day</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
