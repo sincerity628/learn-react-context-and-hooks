347d917ca84b619aacafd6b81325a42607f87aa1
sincerity628 <14785481094@163.com>
2019-12-17 14:42:59 +0800

    hooks learning init.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import BookList from './components/BookList';
-import ThemeToggle from './components/ThemeToggle';
-import ThemeContextProvider from './contexts/ThemeContext';
-import AuthContextProvider from './contexts/AuthContext';
 
 const App = () => {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookList />
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      
     </div>
   );
 }
