af29dc5978b27b9fc4db45506ca6652d7ac24d02
sincerity628 <14785481094@163.com>
2019-12-17 14:52:19 +0800

    added song list.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SongList from './components/SongList';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <SongList />
     </div>
   );
 }
---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>all i do</li>
+        <li>i wish</li>
+        <li>memories</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SongList;
