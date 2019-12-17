938d6e3c92838ddb3cf311feb561b766feeb64f9
sincerity628 <14785481094@163.com>
2019-12-17 15:10:30 +0800

    added use-state hook.

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
+
+const initSongs = [
+  {id: 1, title: 'all i do'},
+  {id: 2, title: 'i wish'},
+  {id: 3, title: 'memories'}
+];
 
 const SongList = () => {
+  const [songs, setSongs] = useState(initSongs);
+
+  const addSong = () => {
+    setSongs([...songs, { id: uuid(), title: 'new song' }]);
+  };
+
   return (
     <div className="song-list">
       <ul>
-        <li>all i do</li>
-        <li>i wish</li>
-        <li>memories</li>
+        { songs.map(song => {
+          return ( <li key={song.id}>{ song.title }</li> );
+        }) }
       </ul>
+      <button onClick={addSong}>add song</button>
     </div>
   );
 }
