56ad6b9aad0cefcf78b1d4c744a8c81e5de85bca
sincerity628 <14785481094@163.com>
2019-12-18 14:37:08 +0800

    book list.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import BookList from './components/BookList';
-import ThemeToggle from './components/ThemeToggle';
-import ThemeContextProvider from './contexts/ThemeContext';
-import AuthContextProvider from './contexts/AuthContext';
-import BookContextProvider from './contexts/BookContext';
 
 const App = () => {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookContextProvider>
-            <BookList />
-          </BookContextProvider>
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+    
     </div>
   );
 }
