ec73b8b9948086b693e44602fb3d186b626f9e74
sincerity628 <14785481094@163.com>
2019-12-18 12:05:49 +0800

    rewrited with hooks.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-
-  render() {
-    const { isLight, light, dark } = this.context;
-    const theme = isLight ? light : dark;
-
-    return (
-      <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
-        <ul>
-          <li style={{ background: theme.ui }}>夏天，烟火和我的尸体</li>
-          <li style={{ background: theme.ui }}>me before you</li>
-          <li style={{ background: theme.ui }}>one day</li>
-        </ul>
-      </div>
-    );
-  }
+const BookList = () => {
+  const { isLight, dark, light } = useContext(ThemeContext);
+  const theme = isLight? light : dark;
+  return (
+    <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
+      <ul>
+        <li style={{ background: theme.ui }}>夏天，烟火和我的尸体</li>
+        <li style={{ background: theme.ui }}>me before you</li>
+        <li style={{ background: theme.ui }}>one day</li>
+      </ul>
+    </div>
+  );
 }
 
 export default BookList;
