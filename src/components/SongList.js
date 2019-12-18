18ef9a6169215e2ebca26247805a4c6f1092227b
sincerity628 <14785481094@163.com>
2019-12-18 11:50:31 +0800

    added use effect hooks.

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import uuid from 'uuid/v1';
 import NewSongForm from './NewSongForm';
 
@@ -10,6 +10,15 @@ const initSongs = [
 
 const SongList = () => {
   const [songs, setSongs] = useState(initSongs);
+  const [age, setAge] = useState(20);
+
+  useEffect(() => {
+    console.log('use effect ran.', songs);
+  }, [songs]);
+
+  useEffect(() => {
+    console.log('use effect ran.', age);
+  }, [age]);
 
   const addSong = (title) => {
     setSongs([...songs, { id: uuid(), title: title }]);
@@ -23,6 +32,7 @@ const SongList = () => {
         }) }
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>add 1 to age.{ age }</button>
     </div>
   );
 }
