fe95caf5823d6a05cb7ff335993275d1fc872906
sincerity628 <14785481094@163.com>
2019-12-19 16:56:09 +0800

    handle warning.

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -3,12 +3,6 @@ import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
-const initBooks = [
-  { id: 1, title: '夏天，烟火和我的尸体', author: '乙一' },
-  { id: 2, title: '挪威的森林', author: '村上春树' },
-  { id: 3, title: '我们仨', author: '杨绛' }
-];
-
 const BookContextProvider = (props) => {
   const [books, dispatch] = useReducer(bookReducer, [], () => {
     const localData = localStorage.getItem('books');
